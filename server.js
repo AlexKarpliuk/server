cce31c1d9bed2436cdd3b65f2a1002af6811839d
unknown <karpliuksasha@gmail.com>
2023-03-27 13:59:52 +0300

    add .env

--- server.js
+++ server.js
@@ -11,15 +11,17 @@ mongoose.set('strictQuery', true);
 const multer = require('multer')
 const uploadMiddleware = multer({ dest: 'uploads/' })
 const fs = require('fs')
-const { info } = require('console')
+require('dotenv').config();
+// const { info } = require('console')
 
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+
+app.use(cors({ credentials: true, origin: process.env.REACT_APP_BASE_CORS_URL}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
-mongoose.connect('mongodb+srv://Alex:300010Sano@portfolio.7au2fgv.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect(process.env.DATABASE_URL);
 
 
 // Generate a salt to add to the hash
@@ -172,4 +174,4 @@ app.get('/post/:id', async (req, res) => {
 
 
 
-app.listen(5000);
+app.listen(process.env.REACT_APP_BASE_URL || 5000);
