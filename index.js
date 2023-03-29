19bf4087874b47d75e29ceedda2bec08706f3b29
unknown <karpliuksasha@gmail.com>
2023-03-29 15:11:21 +0300

    add headers to vercel

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ const connectDB = async () => {
 	}
 }
 
-app.get("/blog", (req, res) => {
-		res.header("Access-Control-Allow-Origin", process.env.REACT_APP_BASE_CORS_URL);
-});
+// app.get("/blog", (req, res) => {
+// 		res.header("Access-Control-Allow-Origin", process.env.REACT_APP_BASE_CORS_URL);
+// });
 
 // Generate a salt to add to the hash
 const salt = bcrypt.genSaltSync(10);
