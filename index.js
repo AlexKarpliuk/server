ad455caf0db8c7864c3570df67b2d2b0d5c08e8a
unknown <karpliuksasha@gmail.com>
2023-04-05 18:09:14 +0300

    add headers for CORS2

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors({
 	credentials: true,
 }));
 app.use((req, res, next) => {
-	// res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	res.setHeader('Access-Control-Allow-Credentials', true);
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
